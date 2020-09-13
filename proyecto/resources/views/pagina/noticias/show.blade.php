b4ce469c0623c456c3e8192f7d60ffa2c339d01e
JL18CG <39381181+JL18CG@users.noreply.github.com>
2020-09-13 16:01:05 -0600

    Pagina Principal-Noticias
    Pagina Principal-Noticias

--- proyecto/resources/views/pagina/noticias/show.blade.php
+++ proyecto/resources/views/pagina/noticias/show.blade.php
@@ -33,7 +33,6 @@
     <!-- Sidebar Widgets Column -->
     <div class="col-md-4">
 
-
     <!-- Search Widget -->
     <div class="card my-4 ">
       <h5 class="card-header">Buscador de Noticias</h5>
